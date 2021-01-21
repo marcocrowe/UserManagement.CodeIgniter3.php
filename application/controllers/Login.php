c4d9ea1f256956c47d02671949769525853b1693
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2021-01-21 03:42:07 +0000

    Login Controller and View

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,6 +7,8 @@ class Login extends CI_Controller
 	public function index()
 	{
 		$this->load->model("UserAccountService");
+		$this->load->model("UserAccountRepository");
+
 		$this->load->model("UserAccountSchema");
 
 
@@ -14,22 +16,25 @@ class Login extends CI_Controller
 	}
 	public function manage()
 	{
+		$this->load->helper('url');
 		$this->load->library("session");
 		$this->load->model("UserAccountService");
 
-
-		$username = $this->input->post($this->UserAccountSchema->Username);
 		$password = $this->input->post($this->UserAccountSchema->Password);
+		$username = $this->input->post($this->UserAccountSchema->Username);
 
 		$userAccount = $this->UserAccountService->getUserAccountByCredentials($username, $password);
 
-		if (isset($userAccount)) {
+		if ($userAccount != null) {
 			$this->session->set_userdata("UserAccountId", $userAccount->Id);
-			redirect('useraccounts/index');
-		}
-		else
-		{
-			$this->load->view("LoginView");
+			redirect(base_url() . "index.php/useraccounts/");
+		} else {
+			$vars = array(
+				"error" => "Incorrect login details entered",
+				"username" => $username
+			);
+			var_dump($vars);
+			$this->load->view("LoginView", $vars);
 		}
 	}
 }
