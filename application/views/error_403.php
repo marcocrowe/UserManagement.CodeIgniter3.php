36b8f5556bf3922366a832ca895f6f6af03da0c2
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2021-02-14 00:49:22 +0000

    Unauthorized Access page

---
+++ application/views/error_403.php
@@ -0,0 +1,8 @@
+<?php $this->load->helper("url"); ?>
+<div class="panel">
+	<h2>Unauthorized Access</h2>
+	<?php if (isset($unauthorizedPath)) {
+		echo "<p>" . $unauthorizedPath . "</p>";
+	} ?>
+	<p><a href="<?php echo site_url("login/"); ?>">Click here to login.</a></p>
+</div>
